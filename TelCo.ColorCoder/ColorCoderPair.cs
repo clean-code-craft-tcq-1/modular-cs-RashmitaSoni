e2e2cafa2686903b5940d61f3e9469f02b216b8f
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-02-21 23:21:29 +0530

    Update ColorCoderPair.cs

--- TelCo.ColorCoder/ColorCoderPair.cs
+++ TelCo.ColorCoder/ColorCoderPair.cs
@@ -1,72 +1,29 @@
 using System;
 using System.Diagnostics;
 using System.Drawing;
-using TelCo.ColorCoder;
 
-namespace TelCo.ColorCoderPair
-{
+namespace TelCo.ColorCoderClasses
+{ /// <summary>
+  /// This class provides the color coding and mapping of Colors number to color and color to Colors number.
+  /// </summary>
+    public class ColorGroup
+    {
+        public static Color[] Major;
+        public static Color[] Minor;
+        ColorPair colorPair = new ColorPair();
+        static ColorGroup()
+        {
+            Major = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            Minor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+    }
     public class ColorPair
     {
-        internal Color majorColor;
-        internal Color minorColor;
+        internal Color majorColor, minorColor;
         public override string ToString()
         {
             return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
         }
-        public static ColorPair FetchColorFromPairNumber(int pairNumber)
-        {
-
-            int minorSize = ColorGroup.Minor.Length;
-            int majorSize = ColorGroup.Major.Length;
-            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
-            {
-                throw new ArgumentOutOfRangeException(
-                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-            }
-
-            int majorIndex = (pairNumber - 1) / minorSize;
-            int minorIndex = (pairNumber - 1) % minorSize;
-
-            ColorPair Colors = new ColorPair()
-            {
-                majorColor = ColorGroup.Major[majorIndex],
-                minorColor = ColorGroup.Minor[minorIndex]
-            };
-
-            return Colors;
-        }
-        public static int FetchPairNumberFromColor(ColorPair Colors)
-        {
-
-            int majorIndex = -1, minorIndex = -1;
-            int PairNumber;
-
-            for (int i = 0; i < ColorGroup.Major.Length; i++)
-            {
-                if (ColorGroup.Major[i] == Colors.majorColor)
-                {
-                    majorIndex = i;
-                    break;
-                }
-            }
-
-            for (int i = 0; i < ColorGroup.Minor.Length; i++)
-            {
-                if (ColorGroup.Minor[i] == Colors.minorColor)
-                {
-                    minorIndex = i;
-                    break;
-                }
-            }
-
-            if (majorIndex == -1 || minorIndex == -1)
-            {
-                throw new ArgumentException(
-                    string.Format("Unknown Colors: {0}", Colors.ToString()));
-            }
-
-            PairNumber = (majorIndex * ColorGroup.Minor.Length) + (minorIndex + 1);
-            return PairNumber;
-        }
     }
+       
 }

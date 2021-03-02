9179f081f88466618495756cb5f7be19fbe46c15
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-03-02 12:57:17 +0530

    Update ColorCoderTest.cs

--- TelCo.ColorCoder/ColorCoderTest.cs
+++ TelCo.ColorCoder/ColorCoderTest.cs
@@ -19,31 +19,27 @@ namespace TelCo.ColorCoderTests
         {
             pairNumber = 4;
             ColorPair testPair1 = ColorCodeMappers.ColorCodeMappers.FetchColorFromPairNumber(pairNumber);
-            PrintColorPairs(testPair1, pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
             Debug.Assert(testPair1.majorColor == Color.White && testPair1.minorColor == Color.Brown);
             pairNumber = 5;
             testPair1 = ColorCodeMappers.ColorCodeMappers.FetchColorFromPairNumber(pairNumber);
-            PrintColorPairs(testPair1, pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
             Debug.Assert(testPair1.majorColor == Color.White && testPair1.minorColor == Color.SlateGray);
             pairNumber = 23;
             testPair1 = ColorCodeMappers.ColorCodeMappers.FetchColorFromPairNumber(pairNumber);
-            PrintColorPairs(testPair1, pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
             Debug.Assert(testPair1.majorColor == Color.Violet && testPair1.minorColor == Color.Green);
         }
         public static void TestFetchPairNumberFromColor()
         {
             ColorPair testPair2 = new ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
             pairNumber = ColorCodeMappers.ColorCodeMappers.FetchPairNumberFromColor(testPair2);
-            PrintColorPairs(testPair2, pairNumber);
+            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
             Debug.Assert(pairNumber == 18);
             testPair2 = new ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
             pairNumber = ColorCodeMappers.ColorCodeMappers.FetchPairNumberFromColor(testPair2);
-            PrintColorPairs(testPair2, pairNumber);
+            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
             Debug.Assert(pairNumber == 6);
-        }
-        public static void PrintColorPairs(ColorPair testPair, int pairNumber)
-        {
-            Console.WriteLine("Colors: {0}, PairNumber: {1}\n", testPair, pairNumber);
         }
     }
 }
